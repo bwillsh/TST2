4ea8d52642b88d6b2a59bb30d79aaa4423f53ae7
bwillsh <bwillsh@umich.edu>
2016-03-09 17:39:24 -0500

    multiple level work

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+
+    public int level;
+    public Vector2 location = new Vector2(0,0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Button()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
