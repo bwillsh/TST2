12c70bebd3667d6c62494518c1e8780e46b2bac1
bwillsh <bwillsh@umich.edu>
2016-03-13 17:16:30 -0400

    Ninjas now spawn correctly

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -6,12 +6,16 @@ public class LevelGen : MonoBehaviour {
     CombatController combat;
 
     public int numWalls = 1;
+    public int numNinjas = 1;
+    public int numJumps = 4;
 
     public GameObject wall;
-    public GameObject ninja;
+    public Ninja ninja;
+    public GameObject empty;
 
     public Vector2 max = new Vector2(18.05f,13.4f);
     public Vector2 min = new Vector2(-8f, -3.1f);
+    Vector2 FootPos;
 
     public static LevelGen S;
     void Awake()
@@ -21,6 +25,7 @@ public class LevelGen : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         combat = GameObject.Find("CombatController").GetComponent<CombatController>();
+        FootPos = Foot.S.transform.position;
     }
 	
 	// Update is called once per frame
@@ -33,13 +38,36 @@ public class LevelGen : MonoBehaviour {
 
     public void GenLevel()
     {
+        float x;
+        float y;
         print("Generating Level");
         for(int i = 0; i < numWalls; i++)
         {
-            float x = Random.Range(min.x, max.x);
-            float y = Random.Range(min.y, max.y);
+            x = Random.Range(min.x, max.x);
+            y = Random.Range(min.y, max.y);
             Instantiate(wall, new Vector3(x, y, -1), Quaternion.Euler(0, 0, Random.Range(0,180)));
-            print("Hi " + i);
+        }
+        for (int i = 0; i < numNinjas; i++)
+        {
+            x = Random.Range(15f, 17);
+            y = Random.Range(min.y, max.y);
+            Ninja clone = (Ninja)Instantiate(ninja, new Vector3(x, y, -1), Quaternion.identity);
+            GameObject npoint = (GameObject)Instantiate(empty, clone.transform.position, Quaternion.identity);
+            clone.jumpPoints.Add(npoint.transform);
+            float jx = x;
+            float dec = (x - Foot.S.transform.position.x) / numJumps;
+            for(int j = 1; j < numJumps; j++)
+            {
+                jx -= dec;
+                float jy = Random.Range(min.y, max.y);
+                GameObject tpoint = (GameObject)Instantiate(empty, new Vector3(jx, jy, -1), Quaternion.identity);
+                clone.jumpPoints.Add(tpoint.transform);
+            }
+            GameObject ttpoint = (GameObject)Instantiate(empty, Foot.S.transform.position, Quaternion.identity);
+            clone.jumpPoints.Add(ttpoint.transform);
+            clone.jumpPoints.Reverse();
+            combat.ninjaList.Add(clone);
+
         }
     }
 }
