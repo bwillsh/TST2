97da8bf727919ae262fc386b07f11bc8ecd5cbd1
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-03-16 19:00:02 -0400

    starting throwninja

---
+++ Assets/Scripts/throwNinja.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwNinja : MonoBehaviour {
+
+	public int turns_left;
+	private CombatController combat; //the combat script that keeps track of the combat flow
+	public ParticleSystem	explosion; //the particle system that makes the ninja explode
+	private Foot			foot;
+
+	// Use this for initialization
+	void Start () {
+
+
+		int starting_turns = turns_left;
+		foot = GameObject.Find ("Foot").GetComponent<Foot>();
+
+		//access variables in combat controller
+		combat = GameObject.Find("CombatController").GetComponent<CombatController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (combat.turn == TurnState.ENEMYSTART)
+		{
+			turns_left--;
+		}
+	}
+}
