a093a685b7e2fb6ca3c40430fd802048c625f5ab
Ryan Whitfield <whitfier@umich.edu>
2016-03-16 22:17:43 -0400

    Inventory work.
    Key now follows foot on retrieval.
    Wait for foot to return to leave scene.
    Display current item in overworked UI.

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour {
+
+	public Sprite keySprite;
+
+	// Use this for initialization
+	void Start () {
+		switch (GameManager.S.currentItem) {
+		case "Key":
+			print ("Display key sprite.");
+			GetComponent<Image> ().sprite = keySprite;
+			break;
+		default:
+			print ("No item.");
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
