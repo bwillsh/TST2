c08a842d815fc8f09f47936c943703425b138e73
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-04-03 18:15:16 -0400

    last changes before beta release

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
-
+	TextMesh tm;
 	public string loadWhichLevel;
 	// Use this for initialization
 	void Start () {
+		tm = GetComponentInChildren<TextMesh>(true);
 	}
 
 	// Update is called once per frame
@@ -17,6 +18,7 @@ public class Door : MonoBehaviour {
     public void Button()
     {
 		MovementController.S.disableControls = true;
+		tm.gameObject.SetActive (true);
         print("Loading Level " + loadWhichLevel);
 		GameManager.S.level = Application.loadedLevel;
 		Application.LoadLevel (loadWhichLevel);
