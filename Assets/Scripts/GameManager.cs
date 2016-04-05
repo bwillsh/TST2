3cd2e6dab4184abced5077cf340b49082a5562ea
ihecker <ihecker@umich.edu>
2016-04-05 10:07:47 -0400

    multi touch disabled

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour {
     void Awake()
     {
         S = this;
+		Input.multiTouchEnabled = false;
         if(Application.loadedLevel != null)
         {
             loader = Application.loadedLevelName;
