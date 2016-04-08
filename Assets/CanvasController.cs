a4eeeff1b48cb6ac3d8561c197781d1bd8b70e78
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-04-07 23:06:45 -0400

    all paths done for symbolstix, prefab for puzzles created

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class CanvasController : MonoBehaviour {
+
+	public int num_answers;
+	public int count;
+	public Canvas well_done;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+
+
+
+	void Update(){
+		if (count == num_answers) {
+			StartCoroutine (wait ());
+		}
+	}
+
+	IEnumerator wait() {
+		well_done.gameObject.SetActive (true);
+		yield return new WaitForSeconds (3);
+		gameObject.SetActive (false);
+	}
+
+	public void increment(){
+		count++;
+	}
+}
