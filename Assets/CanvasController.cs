4fdf4b0bbf79284d2537516b110c5c43bba27434
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-04-10 23:19:35 -0400

    all symbol puzzles completed

--- Assets/CanvasController.cs
+++ Assets/CanvasController.cs
@@ -8,6 +8,7 @@ public class CanvasController : MonoBehaviour {
 	public int num_answers;
 	public int count;
 	public Canvas well_done;
+	public Canvas out_of_time;
 	public string loaded_level;
 
 
@@ -16,12 +17,17 @@ public class CanvasController : MonoBehaviour {
 		count = 0;
 	}
 
+	public float timeLeft = 30.0f;
+	public Text timer;
+	public bool lost = false;
 
-
-	void Update(){
-		if (count == num_answers) {
+	void Update(){		
+		StartCoroutine ("startTimer");
+		if (count == num_answers && !lost) {
+			StopCoroutine ("startTimer");
 			StartCoroutine (win ());
 		}
+
 	}
 
 	IEnumerator win() {
@@ -32,6 +38,22 @@ public class CanvasController : MonoBehaviour {
 		}
 		Application.LoadLevel (loaded_level);
 	}
+
+	IEnumerator startTimer(){
+		yield return new WaitForSeconds (8);
+		timer.gameObject.SetActive (true);
+		timeLeft -= Time.deltaTime;
+		timer.text = "" + Mathf.Round(timeLeft);
+		if(timeLeft < 0){
+			lost = true;
+			timer.text = "0";
+			out_of_time.gameObject.SetActive (true);
+			yield return new WaitForSeconds (3);
+			Application.LoadLevel (loaded_level);
+		}
+
+	}
+
 
 	public void increment(){
 		count++;
