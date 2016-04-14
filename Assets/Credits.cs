1b724c60dfcef4b3222bc8f0c756920dc1e3190a
ihecker <ihecker@umich.edu>
2016-04-13 20:40:19 -0400

    credits scene, untested

---
+++ Assets/Credits.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public string returnToScene;
+	private BoxCollider2D col;
+
+	// Use this for initialization
+	void Start () {
+		col = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Application.LoadLevel(returnToScene);
+		}
+	}
+}
